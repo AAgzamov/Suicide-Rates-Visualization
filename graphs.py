6dff9433f3caf9d7cd66dbd80bf2e66d4e810ff0
Amirkhon <71869548+AAgzamov@users.noreply.github.com>
2021-06-19 13:20:27 +0500

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -6,6 +6,9 @@ import numpy as np
 # reference: https://ourworldindata.org/causes-of-death
 # The share of deaths from injuries, infectious diseases, and non-communicable diseases
 
+# Setting a style sheet.
+plt.style.use('seaborn')
+
 # Creating labels and percentage for deaths by cause in 1990.
 l_1990 = ['Injuries', 'Non-communicable\ndiseases', 'Communicable\ndiseases']
 p_1990 = [9, 58, 33]
@@ -23,32 +26,32 @@ l_2017 = ['Injuries', 'Non-communicable\ndiseases', 'Communicable\ndiseases']
 p_2017 = [8, 73, 19]
 
 # Creating a figure with dimension of 2 rows and 2 columns for subplotting.
-fig, axs = plt.subplots(2, 2, figsize = (11, 9))
+fig, axs = plt.subplots(2, 2, figsize = (11, 9), facecolor='#f2f2f2')
 
 # Creating a bar chart with the label and percentage of 1990 and storing it in variable 'bars_1990'.
-bars_1990 = axs[0, 0].bar(l_1990, p_1990, width = 0.4, color = ['#edf25e', '#3299e3', '#e32b31'], edgecolor = 'black', linewidth = 2)
+bars_1990 = axs[0, 0].bar(l_1990, p_1990, width = 0.4, color = ['#bddb35', '#2de04b', '#265dd4'], edgecolor = 'black', linewidth = 1.5)
 axs[0, 0].set_title('Deaths by cause in the world in 1990', fontdict = {'fontweight': 'bold', 'fontsize':13}) # Setting a title for the bar chart.
 axs[0, 0].set_xlabel('Death causes', fontdict = {'fontstyle': 'italic', 'fontsize': 13}) # Setting xlabel
 axs[0, 0].set_ylabel('Percentage of deaths', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
-bars_1990[0].set_hatch('.') # Setting the hatch for the first bar in the bar chart.
+bars_1990[0].set_hatch('//') # Setting the hatch for the first bar in the bar chart.
 
-bars_2000 = axs[0, 1].bar(l_2000, p_2000, width = 0.4, color = ['#edf25e', '#3299e3', '#e32b31'], edgecolor = 'black', linewidth = 2)
+bars_2000 = axs[0, 1].bar(l_2000, p_2000, width = 0.4, color = ['#bddb35', '#2de04b', '#265dd4'], edgecolor = 'black', linewidth = 1.5)
 axs[0, 1].set_title('Deaths by cause in the world in 2000', fontdict = {'fontweight': 'bold', 'fontsize':13})
 axs[0, 1].set_xlabel('Death causes', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
 axs[0, 1].set_ylabel('Percentage of deaths', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
-bars_2000[0].set_hatch('.')
+bars_2000[0].set_hatch('//')
 
-bars_2010 = axs[1, 0].bar(l_2010, p_2010, width = 0.4, color = ['#edf25e', '#3299e3', '#e32b31'], edgecolor = 'black', linewidth = 2)
+bars_2010 = axs[1, 0].bar(l_2010, p_2010, width = 0.4, color = ['#bddb35', '#2de04b', '#265dd4'], edgecolor = 'black', linewidth = 1.5)
 axs[1, 0].set_title('Deaths by cause in the world in 2010', fontdict = {'fontweight': 'bold', 'fontsize':13})
 axs[1, 0].set_xlabel('Death causes', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
 axs[1, 0].set_ylabel('Percentage of deaths', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
-bars_2010[0].set_hatch('.')
+bars_2010[0].set_hatch('//')
 
-bars_2017 = axs[1, 1].bar(l_2017, p_2017, width = 0.4, color = ['#edf25e', '#3299e3', '#e32b31'], edgecolor = 'black', linewidth = 2)
+bars_2017 = axs[1, 1].bar(l_2017, p_2017, width = 0.4, color = ['#bddb35', '#2de04b', '#265dd4'], edgecolor = 'black', linewidth = 1.5)
 axs[1, 1].set_title('Deaths by cause in the world in 2017', fontdict = {'fontweight': 'bold', 'fontsize':13})
 axs[1, 1].set_xlabel('Death causes', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
 axs[1, 1].set_ylabel('Percentage of deaths', fontdict = {'fontstyle': 'italic', 'fontsize': 13})
-bars_2017[0].set_hatch('.')
+bars_2017[0].set_hatch('//')
 
 
 # Setting yticks for the bar charts.
@@ -60,9 +63,9 @@ for ax in axs.flat:
 #    ax.label_outer()
 
 # Setting the space between each bar chart.
-fig.tight_layout(pad = 4.0)
+fig.tight_layout(pad = 6.0)
 
-#plt.savefig('intro.png', dpi = 150)
+plt.savefig('deaths-by-cause-world.png', dpi = 200)
 plt.show()
 
 
